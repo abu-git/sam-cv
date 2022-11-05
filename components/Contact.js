fae26b31a70cfcacb756d1b0f506a96c92381e30
Samuel Abu <abu.sammie@gmail.com>
2022-11-05 15:05:53 +0200

    footer and contact progress

--- components/Contact.js
+++ components/Contact.js
@@ -1,9 +1,39 @@
 import React, { useState } from 'react'
+import validator from 'validator'
 
 function Contact() {
     const [name, setName] = useState('')
     const [message, setMessage] = useState('')
 
+    //Form validation error state
+    const [errors, setErrors] = useState({})
+
+    //success and failure states
+    const [success, setSuccess] = useState(false)
+    const [failure, setFailure] = useState(false)
+
+    const handleValidation = () => {
+        let tempErrors = {}
+        let isValid = true //default
+
+        if(validator.isEmpty(name)){
+            tempErrors['name'] = true
+            isValid = false
+        }else if(validator.isEmpty(message)){
+            tempErrors['message'] = true
+            isValid = false
+        }
+
+        setErrors({...tempErrors})
+        console.log('errors', errors)
+        return isValid
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        let isValid = handleValidation()
+    }
     
     return (
     <>
@@ -12,12 +42,26 @@ function Contact() {
             <h3 className='text-3xl font-semibold text-center'>Contact</h3>
 
             <div className='flex justify-center mx-6 mt-9'>
-                <form className='w-96'>
+                <form onSubmit={handleSubmit} className='w-96'>
                     <div className='mb-6'>
-                        <input className="appearance-none bg-transparent border-b-2 border-slate-400 w-full text-purple-500 mr-3 py-1 px-2 text-lg leading-tight focus:outline-none focus:border-black dark:focus:border-amber-100" type="text" placeholder="Jane Doe" aria-label="Full name"/>
+                        {errors?.name && (
+                            <p className='text-red-500 my-2'>Name cannot be empty</p>
+                        )}
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)} 
+                            className="appearance-none bg-transparent border-b-2 border-slate-400 w-full text-black dark:text-white mr-3 py-1 px-2 text-xl focus:outline-none focus:border-black dark:focus:border-amber-100" placeholder="Jane Doe" aria-label="Full name"/>
                     </div>
                     <div className='mb-6'>
-                        <textarea rows="8" className="appearance-none bg-transparent border-b-2 border-slate-400 w-full text-purple-500 text-lg mr-3 py-1 px-2 leading-tight focus:outline-none focus:border-black dark:focus:border-amber-100" type="text" placeholder="Your message..." aria-label="Full name" />
+                        {errors?.message && (
+                            <p className='text-red-500 mt-1'>Message cannot be empty</p>
+                        )}
+                        <textarea
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)} 
+                            rows="8" 
+                            className="appearance-none bg-transparent border-b-2 border-slate-400 w-full text-black dark:text-white text-xl mr-3 py-1 px-2 focus:outline-none focus:border-black dark:focus:border-amber-100" type="text" placeholder="Your message..." aria-label="Message" />
                     </div>
                     <div className='mb-4 flex justify-end'>
                         <button className='bg-stone-800 hover:bg-black dark:bg-amber-50 dark:hover:bg-amber-100 shadow-md hover:shadow-lg shadow-cyan-500/60 hover:shadow-purple-500/70 border-2 border-black text-white dark:text-black text-semibold text-lg py-1 px-2 rounded'>Send Message!</button>
--- components/Footer.js
+++ components/Footer.js
@@ -5,33 +5,45 @@ function Footer() {
   return (
     <div className='bg-amber-50 text-black dark:bg-black dark:text-white px-8 py-10'>
         <div className='flex justify-center'>
-            <Image
-                src="/LinkedIn-1.png"
-                width={40}
-                height={40}
-                alt="linkedIn"
-            />
-            <Image
-                src="/Twitter-1.png"
-                width={40}
-                height={40}
-                alt="twitter"
-                className='ml-2'
-            /> 
-            <Image
-                src="/Facebook-1.png"
-                width={40}
-                height={40}
-                alt="linkedIn"
-                className='ml-2'
-            />
-            <Image
-                src="/Instagram-1.png"
-                width={40}
-                height={40}
-                alt="twitter"
-                className='ml-2'
-            />  
+            <a target="_blank" href="https://www.linkedin.com/in/abu-samuel/" rel="noreferrer">
+                <Image
+                    src="/LinkedIn-1.png"
+                    width={40}
+                    height={40}
+                    alt="linkedIn"
+                />
+            </a>
+            
+            <a target="_blank" href="https://twitter.com/reachMHP" rel="noreferrer">
+                <Image
+                    src="/Twitter-1.png"
+                    width={40}
+                    height={40}
+                    alt="twitter"
+                    className='ml-2'
+                /> 
+            </a>
+            
+            <a target="_blank" href="https://www.facebook.com/profile.php?id=1150817536" rel="noreferrer">
+                <Image
+                    src="/Facebook-1.png"
+                    width={40}
+                    height={40}
+                    alt="facebook"
+                    className='ml-2'
+                />
+            </a>
+            
+            <a target="_blank" href='https://www.instagram.com/mhp_samuel/' rel='noreferrer'>
+                <Image
+                    src="/Instagram-1.png"
+                    width={40}
+                    height={40}
+                    alt="twitter"
+                    className='ml-2'
+                />  
+            </a>
+            
         </div>
         <h3 className='text-lg font-light text-center mt-7'>Design by Samuel Abu</h3>
     </div>
