cd628139455c746ac3de5a110b563414adda2f9e
Samuel Abu <abu.sammie@gmail.com>
2022-11-05 04:32:13 +0200

    favicon added

--- components/Landing.js
+++ components/Landing.js
@@ -8,7 +8,7 @@ function Landing() {
         <div name="home"></div>
         <div className='grid grid-cols-1 md:grid-cols-2 items-center mt-16 pb-9 px-10 bg-amber-100 dark:bg-stone-900'>
             <div className='flex justify-center h-80'>
-                <Image src={kit9} alt="Sam landing svg" width={430} height={330} />
+                <Image src={kit9} alt="Sam landing svg" width={470} height={370} />
             </div>
             <div className='md:px-10'>
                 <h2 className='font-extrabold text-5xl md:text-6xl text-center text-black dark:text-white'>Developer and Creative</h2>
