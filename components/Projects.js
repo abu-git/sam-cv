0273c26caad15de5f9132e2fcc9b0602c8e73fe8
Samuel Abu <abu.sammie@gmail.com>
2023-01-12 15:41:12 +0200

    projects card edit

--- components/Projects.js
+++ components/Projects.js
@@ -8,26 +8,6 @@ function Projects() {
         <div className='bg-amber-50 text-black dark:bg-black dark:text-white px-10 md:px-20 py-9'>
             <h3 className='text-4xl font-semibold text-center'>Projects</h3>
             <main className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-20'>
-                <a target='_blank' href='https://www.potiphar.org/' rel='noreferrer'>
-                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden'>
-                        <div className='relative'>
-                            <Image 
-                                width={433}
-                                height={433}
-                                alt="potiphar"
-                                src="/potiphar.jpg"
-                                className='brightness-[0.2] w-full'
-                            />
-                            <div className='absolute -mt-36 md:-mt-48 w-full'>
-                                <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>Potiphar</h4>
-                            </div>
-                        </div>
-                        <span className='text-lg px-3 py-2 text-justify'>
-                            Multi Page web application for a branding and digital marketing business.
-                        </span>
-                    </div>
-                </a>
-
                 <a target='_blank' href='https://www.tunesketch.com/' rel='noreferrer'>
                     <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden'>
                         <div className='relative'>
@@ -42,9 +22,49 @@ function Projects() {
                                 <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>tunesketch</h4>
                             </div>
                         </div>
-                        <span className='text-lg px-3 py-2 text-justify'>
-                            Multi Page web application with a MongoDB database for a music review entity.
-                        </span>
+                        <div className='flex flex-col justify-between'>
+                            <span className='text-lg px-3 py-2 text-justify'>
+                                Multi Page web application with a MongoDB database for a music review entity.
+                            </span>
+                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW APP</h4>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </a>
+
+                <a target='_blank' href='https://www.potiphar.org/' rel='noreferrer'>
+                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden'>
+                        <div className='relative'>
+                            <Image 
+                                width={433}
+                                height={433}
+                                alt="potiphar"
+                                src="/potiphar.jpg"
+                                className='brightness-[0.2] w-full'
+                            />
+                            <div className='absolute -mt-36 md:-mt-48 w-full'>
+                                <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>Potiphar</h4>
+                            </div>
+                        </div>
+                        <div className='flex flex-col justify-between'>
+                            <span className='text-lg px-3 py-2 text-justify'>
+                                Multi Page web application for a branding and digital marketing business.
+                            </span>
+                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW APP</h4>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>                      
                     </div>
                 </a>
 
@@ -62,9 +82,20 @@ function Projects() {
                                 <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>Tashako Mobile</h4>
                             </div>
                         </div>
-                        <span className='text-lg px-3 py-2 text-justify'>
-                            Single Page web application demo for a tech retail business.
-                        </span>
+                        
+                        <div className='flex flex-col justify-between'>
+                            <span className='text-lg px-3 py-2 text-justify'>
+                                Single Page web application demo for a tech retail business.
+                            </span>
+                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW APP</h4>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>        
                     </div>
                 </a>
                 
@@ -83,9 +114,20 @@ function Projects() {
                                 <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>Mahbuub Metals</h4>
                             </div>
                         </div>
-                        <span className='text-lg px-3 py-2 text-justify'>
-                            Single Page web application for an eco-friendly recycling business.
-                        </span>
+                        
+                        <div className='flex flex-col justify-between'>
+                            <span className='text-lg px-3 py-2 text-justify'>
+                                Single Page web application for an eco-friendly recycling business.
+                            </span>
+                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW APP</h4>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
                     </div>
                 </a>
                 
@@ -103,9 +145,20 @@ function Projects() {
                                 <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>iamMHP</h4>
                             </div>
                         </div>
-                        <span className='text-lg px-3 py-2 text-justify'>
-                            Multi Page web application for a musician catalogue.
-                        </span>
+                        
+                        <div className='flex flex-col justify-between'>
+                            <span className='text-lg px-3 py-2 text-justify'>
+                                Multi Page web application for a musician catalogue.
+                            </span>
+                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW APP</h4>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
                     </div>
                 </a>
                 
@@ -123,9 +176,20 @@ function Projects() {
                                 <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>BigBoyEllis</h4>
                             </div>
                         </div>
-                        <span className='text-lg px-3 py-2 text-justify'>
-                            Single Page web application for a musician.
-                        </span>
+                        
+                        <div className='flex flex-col justify-between'>
+                            <span className='text-lg px-3 py-2 text-justify'>
+                                Single Page web application for a musician.
+                            </span>
+                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW APP</h4>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
                     </div>   
                 </a>
                 
