3396c5946b862af395eae39c4159135188b2e7c5
Samuel Abu <abu.sammie@gmail.com>
2023-01-15 11:49:36 +0200

    whatsapp widget module installed

---
+++ components/Whatsapp.js
@@ -0,0 +1,15 @@
+import { WhatsAppWidget } from 'react-whatsapp-widget'
+import 'react-whatsapp-widget/dist/index.css'
+
+import React from 'react'
+
+function Whatsapp() {
+  return (
+    <div className='dark:text-black'>
+        <WhatsAppWidget phoneNumber="27832459000" message="Hi! 👋🏼 What can I do for you?" />
+    </div>
+    
+  )
+}
+
+export default Whatsapp
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Education from '../components/Education'
 import Projects from '../components/Projects'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
+import Whatsapp from '../components/Whatsapp'
 
 
 export default function Home() {
