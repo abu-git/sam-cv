2f3991c6f99e475a542af279bc05394b7349209a
Samuel Abu <abu.sammie@gmail.com>
2023-04-25 11:26:12 +0200

    project card height responsive

--- components/Projects.js
+++ components/Projects.js
@@ -9,21 +9,21 @@ function Projects() {
             <h3 className='text-4xl font-semibold text-center'>Projects</h3>
             <main className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-20'>
                 <a target='_blank' href='https://www.td-inspections.com/' rel='noreferrer'>
-                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[38rem]'>
+                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[30rem] lg:h-[38rem]'>
                         <div className='relative'>
                             <Image 
                                 width={433}
                                 height={433}
                                 alt="TD Inspections"
                                 src="/tdinspections.jpg"
-                                className='brightness-[0.4] w-full'
+                                className='brightness-[0.2] w-full'
                             />
                             <div className='absolute -mt-36 md:-mt-48 w-full'>
                                 <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>TD Inspections</h4>
                             </div>
                         </div>
                         <div className='flex flex-col justify-between'>
-                            <span className='text-lg px-3 py-2 text-justify'>
+                            <span className='text-lg px-3 py-2 text-center'>
                                 Multi Page web application for a home inspections company.
                             </span>
                             <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
@@ -39,7 +39,7 @@ function Projects() {
                 </a>
 
                 <a target='_blank' href='https://www.tasedesign.com/' rel='noreferrer'>
-                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[38rem]'>
+                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[30rem] lg:h-[38rem]'>
                         <div className='relative'>
                             <Image 
                                 width={433}
@@ -53,7 +53,7 @@ function Projects() {
                             </div>
                         </div>
                         <div className='flex flex-col justify-between'>
-                            <span className='text-lg px-3 py-2 text-justify'>
+                            <span className='text-lg px-3 py-2 text-center'>
                                 Multi Page web application with a loader for better User Experience for a design company.
                             </span>
                             <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
@@ -69,7 +69,7 @@ function Projects() {
                 </a>
 
                 <a target='_blank' href='https://www.tunesketch.com/' rel='noreferrer'>
-                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[38rem]'>
+                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[30rem] lg:h-[38rem]'>
                         <div className='relative'>
                             <Image 
                                 width={433}
@@ -83,7 +83,7 @@ function Projects() {
                             </div>
                         </div>
                         <div className='flex flex-col justify-between'>
-                            <span className='text-lg px-3 py-2 text-justify'>
+                            <span className='text-lg px-3 py-2 text-center'>
                                 Multi Page web application with a MongoDB database for a music review entity.
                             </span>
                             <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
@@ -99,7 +99,7 @@ function Projects() {
                 </a>
 
                 <a target='_blank' href='https://www.potiphar.org/' rel='noreferrer'>
-                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[38rem]'>
+                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[30rem] lg:h-[38rem]'>
                         <div className='relative'>
                             <Image 
                                 width={433}
@@ -113,7 +113,7 @@ function Projects() {
                             </div>
                         </div>
                         <div className='flex flex-col justify-between'>
-                            <span className='text-lg px-3 py-2 text-justify'>
+                            <span className='text-lg px-3 py-2 text-center'>
                                 Multi Page web application for a branding and digital marketing business.
                             </span>
                             <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
@@ -129,7 +129,7 @@ function Projects() {
                 </a>
 
                 <a target='_blank' href='https://tashakomobile.vercel.app/' rel='noreferrer'>
-                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[38rem]'>
+                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[30rem] lg:h-[38rem]'>
                         <div className='relative'>
                             <Image 
                                 width={433}
@@ -144,7 +144,7 @@ function Projects() {
                         </div>
                         
                         <div className='flex flex-col justify-between'>
-                            <span className='text-lg px-3 py-2 text-justify'>
+                            <span className='text-lg px-3 py-2 text-center'>
                                 Single Page web application demo for a tech retail business.
                             </span>
                             <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
@@ -161,7 +161,7 @@ function Projects() {
                 
 
                 <a target='_blank' href='https://www.mahbuubmetals.com' rel='noreferrer'>
-                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[38rem]'>
+                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[30rem] lg:h-[38rem]'>
                         <div className='relative'>
                             <Image 
                                 width={433}
@@ -176,7 +176,7 @@ function Projects() {
                         </div>
                         
                         <div className='flex flex-col justify-between'>
-                            <span className='text-lg px-3 py-2 text-justify'>
+                            <span className='text-lg px-3 py-2 text-center'>
                                 Single Page web application for an eco-friendly recycling business.
                             </span>
                             <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
@@ -192,7 +192,7 @@ function Projects() {
                 </a>
                 
                 <a target='_blank' href='https://www.iammhp.com' rel='noreferrer'>
-                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[38rem]'>
+                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[30rem] lg:h-[38rem]'>
                         <div className='relative'>
                             <Image 
                                 width={433}
@@ -207,7 +207,7 @@ function Projects() {
                         </div>
                         
                         <div className='flex flex-col justify-between'>
-                            <span className='text-lg px-3 py-2 text-justify'>
+                            <span className='text-lg px-3 py-2 text-center'>
                                 Multi Page web application for a musician catalogue.
                             </span>
                             <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
@@ -223,7 +223,7 @@ function Projects() {
                 </a>
                 
                 <a target='_blank' href='https://www.bigboyellis.com' rel='noreferrer'>
-                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[38rem]'>
+                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[30rem] lg:h-[38rem]'>
                         <div className='relative'>
                             <Image 
                                 width={433}
@@ -238,7 +238,7 @@ function Projects() {
                         </div>
                         
                         <div className='flex flex-col justify-between'>
-                            <span className='text-lg px-3 py-2 text-justify'>
+                            <span className='text-lg px-3 py-2 text-center'>
                                 Single Page web application for a musician.
                             </span>
                             <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
