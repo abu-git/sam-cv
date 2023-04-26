f6459849f291d5eb8246ca3f84d161a9660bb61f
Samuel Abu <abu.sammie@gmail.com>
2023-04-26 10:24:45 +0200

    remo added

--- components/Projects.js
+++ components/Projects.js
@@ -8,6 +8,36 @@ function Projects() {
         <div className='bg-amber-50 text-black dark:bg-black dark:text-white px-10 md:px-20 lg:px-40 py-9'>
             <h3 className='text-4xl font-semibold text-center'>Projects</h3>
             <main className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-20'>
+                <a target='_blank' href='https://www.remomogatosi.com/' rel='noreferrer'>
+                    <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[32rem] lg:h-[38rem]'>
+                        <div className='relative'>
+                            <Image 
+                                width={433}
+                                height={433}
+                                alt="Remo Mogatosi"
+                                src="/remo.jpg"
+                                className='brightness-[0.2] w-full'
+                            />
+                            <div className='absolute -mt-36 md:-mt-48 w-full'>
+                                <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>Remo</h4>
+                            </div>
+                        </div>
+                        <div className='flex flex-col justify-between'>
+                            <span className='text-lg px-3 py-2 text-center'>
+                                Web Application for a Researcher and Analyst
+                            </span>
+                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW APP</h4>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </a>
+
                 <a target='_blank' href='https://www.td-inspections.com/' rel='noreferrer'>
                     <div className='flex flex-col cursor-pointer bg-amber-100 dark:bg-stone-800 rounded drop-shadow-lg overflow-hidden h-[32rem] lg:h-[38rem]'>
                         <div className='relative'>
