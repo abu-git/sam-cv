3e2af7e8be091854a1a911822658b7e835750f28
Samuel Abu <abu.sammie@gmail.com>
2023-05-17 13:32:02 +0200

    Education component framer motion fx added

--- components/Education.js
+++ components/Education.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 function Education() {
   return (
     <>
         <div name="education"></div>
         <div className='px-8 md:px-14 py-9 bg-amber-100 text-black dark:bg-stone-900 dark:text-white'>
-            <h3 className='text-4xl font-semibold text-center'>Education</h3>
+            <motion.h3 
+                initial={{y:-30, opacity: 0}}
+                whileInView={{y:0, opacity: 1}}
+                transition={{delay: 0.5}}
+                className='text-4xl font-semibold text-center'>Education</motion.h3>
             <main className='mt-7 grid grid-cols-1 md:grid-cols-2 gap-8'>
-                <div className='w-full overflow-hidden rounded-lg shadow-lg flex bg-amber-50 dark:bg-stone-700'>
+                <motion.div 
+                    initial={{y:30, opacity: 0}}
+                    whileInView={{y:0, opacity: 1}}
+                    transition={{delay: 0.7}}
+                    className='w-full overflow-hidden rounded-lg shadow-lg flex bg-amber-50 dark:bg-stone-700'>
                     <div className='w-1/3'>
                         <Image 
                             src="/hyperion.png"
@@ -21,9 +30,13 @@ function Education() {
                         <h4 className='text-xl font-semibold leading-5 mb-1'>Full Stack Web Developer Bootcamp</h4>
                         <h3 className='text-lg leading-5'>HyperionDev CapeTown, South Africa</h3>
                     </section>
-                </div>
+                </motion.div>
 
-                <div className='w-full overflow-hidden rounded-lg shadow-lg flex bg-amber-50 dark:bg-stone-700'>
+                <motion.div 
+                    initial={{y:30, opacity: 0}}
+                    whileInView={{y:0, opacity: 1}}
+                    transition={{delay: 0.9}}
+                    className='w-full overflow-hidden rounded-lg shadow-lg flex bg-amber-50 dark:bg-stone-700'>
                     <div className='w-1/3'>
                         <Image 
                             src="/uwcLogo.png"
@@ -36,10 +49,14 @@ function Education() {
                         <h4 className='text-xl font-semibold leading-5 mb-1'>Bachelor of Science in Computer Science</h4>
                         <h3 className='text-lg leading-5'>University of the Western Cape</h3>
                     </section>
-                </div>
+                </motion.div>
             </main>
             <div className='flex justify-center mt-9'>
-                <div className='w-full md:w-1/2 overflow-hidden rounded-lg shadow-lg flex bg-amber-50 dark:bg-stone-700'>
+                <motion.div 
+                    initial={{y:30, opacity: 0}}
+                    whileInView={{y:0, opacity: 1}}
+                    transition={{delay: 1.1}}
+                    className='w-full md:w-1/2 overflow-hidden rounded-lg shadow-lg flex bg-amber-50 dark:bg-stone-700'>
                     <div className='w-1/3'>
                         <Image 
                             src="/udsslogo.jpg"
@@ -52,7 +69,7 @@ function Education() {
                         <h4 className='text-xl font-semibold leading-5 mb-1'>Senior School Certificate (NECO)</h4>
                         <h3 className='text-lg leading-5'>UDSS Uniport, Nigeria</h3>
                     </section>
-                </div>
+                </motion.div>
             </div>
         </div>
     </>
