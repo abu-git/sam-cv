b90e278a89ba9296e21f3dde28b18883f006732f
Samuel Abu <abu.sammie@gmail.com>
2023-05-17 12:01:17 +0200

    framer-motion installed

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
+import { AnimatePresence } from 'framer-motion'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <AnimatePresence>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AnimatePresence>
+    
   )
 }
 
