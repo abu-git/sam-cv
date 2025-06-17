d5d2d7c382c3e06277dedfc915c074c05a20bf80
Samuel Abu <abu.sammie@gmail.com>
2025-06-17 17:50:48 +0200

    projects web card button edits

--- components/Projects.js
+++ components/Projects.js
@@ -127,10 +127,10 @@ function Projects() {
                             <span className='text-lg px-3 py-2 text-center'>
                                 Researcher and Analyst
                             </span>
-                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
-                                <h4>VIEW APP</h4>
+                            <button className='flex justify-center items-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW WEBSITE</h4>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 w-7 h-7">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                                     </svg>
                                 </span>
@@ -161,10 +161,10 @@ function Projects() {
                             <span className='text-lg px-3 py-2 text-center'>
                                 A home inspections company.
                             </span>
-                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
-                                <h4>VIEW APP</h4>
+                            <button className='flex justify-center items-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW WEBSITE</h4>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 w-7 h-7">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                                     </svg>
                                 </span>
@@ -195,10 +195,10 @@ function Projects() {
                             <span className='text-lg px-3 py-2 text-center'>
                                 A design company.
                             </span>
-                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
-                                <h4>VIEW APP</h4>
+                            <button className='flex justify-center items-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW WEBSITE</h4>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 w-7 h-7">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                                     </svg>
                                 </span>
@@ -229,16 +229,86 @@ function Projects() {
                             <span className='text-lg px-3 py-2 text-center'>
                                 African Music Blog.
                             </span>
-                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
-                                <h4>VIEW APP</h4>
+                            <button className='flex justify-center items-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW WEBSITE</h4>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 w-7 h-7">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                                     </svg>
                                 </span>
                             </button>
                         </div>
                     </motion.div>
+                </a>
+
+                <a target='_blank' href='https://www.iammhp.com' rel='noreferrer'>
+                    <motion.div 
+                        initial={{y:40, opacity: 0}}
+                        whileInView={{y:0, opacity: 1}}
+                        transition={{delay: 0.8}}
+                        className='flex flex-col cursor-pointer bg-amber-50 dark:bg-black rounded drop-shadow-lg overflow-hidden '>
+                        <div className='relative'>
+                            <Image 
+                                width={433}
+                                height={433}
+                                alt="iamMHP"
+                                src="/iammhp.jpg"
+                                className='brightness-[0.2] w-full'
+                            />
+                            <div className='absolute -mt-36 md:-mt-40 w-full'>
+                                <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>iamMHP</h4>
+                            </div>
+                        </div>
+                        
+                        <div className='flex flex-col justify-between'>
+                            <span className='text-lg px-3 py-2 text-center'>
+                                A musician catalogue.
+                            </span>
+                            <button className='flex justify-center items-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW WEBSITE</h4>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 w-7 h-7">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
+                    </motion.div>
+                </a>
+                
+                <a target='_blank' href='https://www.bigboyellis.com' rel='noreferrer'>
+                    <motion.div 
+                        initial={{y:40, opacity: 0}}
+                        whileInView={{y:0, opacity: 1}}
+                        transition={{delay: 0.9}}
+                        className='flex flex-col cursor-pointer bg-amber-50 dark:bg-black rounded drop-shadow-lg overflow-hidden '>
+                        <div className='relative'>
+                            <Image 
+                                width={433}
+                                height={433}
+                                alt="bigboyellis"
+                                src="/bigboyellis.jpg"
+                                className='brightness-[0.2] w-full'
+                            />
+                            <div className='absolute -mt-36 md:-mt-40 w-full'>
+                                <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>BigBoyEllis</h4>
+                            </div>
+                        </div>
+                        
+                        <div className='flex flex-col justify-between'>
+                            <span className='text-lg px-3 py-2 text-center'>
+                                A musician website.
+                            </span>
+                            <button className='flex justify-center items-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW WEBSITE</h4>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 w-7 h-7">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
+                    </motion.div>   
                 </a>
 
                 <a target='_blank' href='https://potiphar.vercel.app/' rel='noreferrer'>
@@ -263,10 +333,10 @@ function Projects() {
                             <span className='text-lg px-3 py-2 text-center'>
                                 Digital Marketing Business.
                             </span>
-                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
-                                <h4>VIEW APP</h4>
+                            <button className='flex justify-center items-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW WEBSITE</h4>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 w-7 h-7">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                                     </svg>
                                 </span>
@@ -298,10 +368,10 @@ function Projects() {
                             <span className='text-lg px-3 py-2 text-center'>
                                 Fictional tech retail business.
                             </span>
-                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
-                                <h4>VIEW APP</h4>
+                            <button className='flex justify-center items-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW WEBSITE</h4>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 w-7 h-7">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                                     </svg>
                                 </span>
@@ -334,86 +404,16 @@ function Projects() {
                             <span className='text-lg px-3 py-2 text-center'>
                                Eco-friendly recycling business.
                             </span>
-                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
-                                <h4>VIEW APP</h4>
+                            <button className='flex justify-center items-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
+                                <h4>VIEW WEBSITE</h4>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 w-7 h-7">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                                     </svg>
                                 </span>
                             </button>
                         </div>
                     </motion.div>
-                </a>
-                
-                <a target='_blank' href='https://www.iammhp.com' rel='noreferrer'>
-                    <motion.div 
-                        initial={{y:40, opacity: 0}}
-                        whileInView={{y:0, opacity: 1}}
-                        transition={{delay: 0.8}}
-                        className='flex flex-col cursor-pointer bg-amber-50 dark:bg-black rounded drop-shadow-lg overflow-hidden '>
-                        <div className='relative'>
-                            <Image 
-                                width={433}
-                                height={433}
-                                alt="iamMHP"
-                                src="/iammhp.jpg"
-                                className='brightness-[0.2] w-full'
-                            />
-                            <div className='absolute -mt-36 md:-mt-40 w-full'>
-                                <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>iamMHP</h4>
-                            </div>
-                        </div>
-                        
-                        <div className='flex flex-col justify-between'>
-                            <span className='text-lg px-3 py-2 text-center'>
-                                A musician catalogue.
-                            </span>
-                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
-                                <h4>VIEW APP</h4>
-                                <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                                    </svg>
-                                </span>
-                            </button>
-                        </div>
-                    </motion.div>
-                </a>
-                
-                <a target='_blank' href='https://www.bigboyellis.com' rel='noreferrer'>
-                    <motion.div 
-                        initial={{y:40, opacity: 0}}
-                        whileInView={{y:0, opacity: 1}}
-                        transition={{delay: 0.9}}
-                        className='flex flex-col cursor-pointer bg-amber-50 dark:bg-black rounded drop-shadow-lg overflow-hidden '>
-                        <div className='relative'>
-                            <Image 
-                                width={433}
-                                height={433}
-                                alt="bigboyellis"
-                                src="/bigboyellis.jpg"
-                                className='brightness-[0.2] w-full'
-                            />
-                            <div className='absolute -mt-36 md:-mt-40 w-full'>
-                                <h4 className='brightness-100 font-bold text-4xl text-white px-5 text-center'>BigBoyEllis</h4>
-                            </div>
-                        </div>
-                        
-                        <div className='flex flex-col justify-between'>
-                            <span className='text-lg px-3 py-2 text-center'>
-                                A musician website.
-                            </span>
-                            <button className='flex justify-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-500 dark:text-gray-500'>
-                                <h4>VIEW APP</h4>
-                                <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                                    </svg>
-                                </span>
-                            </button>
-                        </div>
-                    </motion.div>   
                 </a>
                 
             </main>
