dc4f259547622b1787d9c588a5b06c683b0474c7
Samuel Abu <45131368+abu-git@users.noreply.github.com>
2026-02-25 16:29:03 +0200

    Update Projects.js
    iamMHP url changes

--- components/Projects.js
+++ components/Projects.js
@@ -241,7 +241,7 @@ function Projects() {
                     </motion.div>
                 </a>
 
-                <a target='_blank' href='https://www.iammhp.com' rel='noreferrer'>
+                <a target='_blank' href='https://iammhp.vercel.app/' rel='noreferrer'>
                     <motion.div 
                         initial={{y:40, opacity: 0}}
                         whileInView={{y:0, opacity: 1}}
